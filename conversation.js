6421c804525e229471390c24ab62ec95d2399269
Shivam Mishra <scm.mymail@gmail.com>
2023-06-14 16:38:38 +0530

    feat: setup conversation.js

---
+++ conversation.js
@@ -0,0 +1,21 @@
+import http from "k6/http";
+import { sleep } from "k6";
+
+const hostname = __ENV.HOSTNAME || "staging.chatwoot.com";
+const account_id = __ENV.ACCOUNT_ID || 1;
+
+export const options = {
+  iterations: 10, // run it 10 times, single VU
+  throw: true,
+};
+
+export default function () {
+  const url = `https://${hostname}/api/v1/accounts/${account_id}/conversations`;
+  http.get(url, {
+    headers: {
+      api_access_token: __ENV.API_ACCESS_TOKEN,
+      "Content-Type": "application/json",
+    },
+  });
+  sleep(2);
+}
